0852cb3c92d218dc2b3625cd9a9b977d9d7730eb
andipchauhandev <andipchauhan.dev@gmail.com>
2025-02-22 09:42:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "docker" {}
+
 
 resource "docker_container" "nginx" {
   image = "nginx"
